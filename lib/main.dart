8f0550fb204b0feefe032c04af3389f05b97649e
suphakornpu <63160101@go.buu.ac.th>
2023-03-02 12:33:10 +0700

    add file add.dart, chat_list.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// ตัวอย่างการ import สำหรับเทสหน้าจอของตัวเอง
-// import 'package:chatbot_frontend/pages/user/chat.dart'; // หน้า view ของตัวเอง
+import 'package:chatbot_frontend/pages/user/chat_list.dart';
 void main() {
   runApp(MyApp());
 }
@@ -9,14 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Dialogflow Flutter',
+      title: 'main',
       theme: new ThemeData(
         fontFamily: 'Inter',
         primarySwatch: Colors.deepOrange,
       ),
       debugShowCheckedModeBanner: false,
       // ตัวอย่าง  สำหรับเทสหน้าจอของตัวเอง
-      // home: ChatPage(title: ''), //เอาชื่อคลาสมาใส่
+      home: ChatList(), //เอาชื่อคลาสมาใส่
     );
   }
 }
--- lib/pages/user/chat_list.dart
+++ lib/pages/user/chat_list.dart
@@ -1,1 +1,79 @@
-//page for chat list
+import 'package:flutter/material.dart';
+import 'package:chatbot_frontend/pages/user/add.dart';
+
+class ChatList extends StatefulWidget {
+  // const ChatList({Key? key}) : super(key: key);
+
+  @override
+  State<ChatList> createState() => _ChatListState();
+}
+
+class _ChatListState extends State<ChatList> {
+  List dataListItem = [
+    'ขอใบเบิกเงินเดือน',
+    'ขอขั้นตอนการยื่นเอกสารขอใบเบิกเงินเดือน'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All Q & A"),
+      ),
+      body: listData(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddPage()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget listData() {
+    return ListView.builder(
+      itemCount: dataListItem.length,
+      itemBuilder: (BuildContext ctx, index) {
+        // Display the list item
+        return Dismissible(
+          key: UniqueKey(),
+
+          // only allows the user swipe from right to left
+          direction: DismissDirection.endToStart,
+
+          // Remove this title from the list
+          // In production enviroment, you may want to send some request to delete it on server side
+          onDismissed: (_) {
+            setState(() {
+              dataListItem.removeAt(index);
+            });
+            final snackBar = SnackBar(
+              content: const Text('delete completed'),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+
+          // This will show up when the user performs dismissal action
+          // It is a red background and a trash icon
+          background: Container(
+            color: Colors.red,
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            alignment: Alignment.centerRight,
+            child: const Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ),
+
+          // Display item's title
+          child: Card(
+            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            child: ListTile(
+              title: Text(dataListItem[index]),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
